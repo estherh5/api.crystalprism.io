65bfbffe6e0fafcf9c23554cdb61de4310387f2e
estherh5 <hoffman.esther@gmail.com>
2017-08-25 23:10:07 -0400

    Add server file to store and retrieve leaders for Rhythm of Life

---
+++ rhythm_of_life/rhythm_of_life.py
@@ -0,0 +1,22 @@
+import csv
+import json
+import os
+
+from flask import jsonify, request
+from operator import itemgetter
+
+
+def add_leader():
+    data = request.get_json()
+    with open(os.path.dirname(__file__) + '/leaders.json') as leaders_file:
+        content = json.load(leaders_file)
+        content.append(data)
+    with open(os.path.dirname(__file__) + '/leaders.json', 'w') as leaders_file:
+        json.dump(content, leaders_file)
+    return 'Success!'
+
+def get_leaders():
+    with open(os.path.dirname(__file__) + '/leaders.json', 'r') as leaders_file:
+        leaders = json.load(leaders_file)
+        leaders.sort(key = itemgetter('score'), reverse = True)
+        return jsonify(leaders[0:4])
