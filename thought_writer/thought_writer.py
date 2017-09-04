e79859f5b75d468481727c9c1a32526838d6f67f
estherh5 <hoffman.esther@gmail.com>
2017-09-04 17:05:01 -0400

    Update thought_writer endpoint to store post data in user file

--- thought_writer/thought_writer.py
+++ thought_writer/thought_writer.py
@@ -26,6 +26,17 @@ def add_entry():
         data = [data]
     with open(os.path.dirname(__file__) + '/' + writer + '.json', 'w') as thoughts_file:
         json.dump(data, thoughts_file)
+    with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/user/users.json') as users_file:
+        user_data = json.load(users_file)
+        for info in user_data:
+            if info['username'].lower() == writer.lower():
+                posts = int(info['post_number']) + 1
+                updated_data = {'username': info['username'], 'password': info['password'], 'salt': info['salt'], 'first_name': info['first_name'], 'last_name': info['last_name'], 'name_public': info['name_public'], 'email': info['email'], 'email_public': info['email_public'], 'color': info['color'], 'about': info['about'], 'admin': info['admin'], 'member_since': info['member_since'], 'shapes_plays': info['shapes_plays'], 'shapes_scores': info['shapes_scores'], 'shapes_high_score': info['shapes_high_score'], 'rhythm_plays': info['rhythm_plays'], 'rhythm_scores': info['rhythm_scores'], 'rhythm_high_score': info['rhythm_high_score'], 'rhythm_high_lifespan': info['rhythm_high_lifespan'], 'images': info['images'], 'liked_images': info['liked_images'], 'post_number': posts}
+                user_data = [info for info in user_data if info['username'].lower() != writer.lower()]
+        user_data.append(updated_data)
+        updated_data = user_data
+    with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/user/users.json', 'w') as users_file:
+        json.dump(updated_data, users_file)
     return make_response('Success', 200)
 
 def get_entry():
@@ -36,9 +47,7 @@ def get_entry():
     writer = payload['username']
     with open(os.path.dirname(__file__) + '/' + writer + '.json', 'r') as thoughts_file:
         thought_entries = json.load(thoughts_file)
-        print(thought_entries)
         for entry in thought_entries:
-            print(int(request.args.get('timestamp')))
             if entry['timestamp'] == int(request.args.get('timestamp')):
                 return jsonify(entry)
             else:
@@ -55,6 +64,17 @@ def del_entry():
         thought_entries = [entry for entry in thought_entries if entry['timestamp'] != int(request.args.get('timestamp'))]
     with open(os.path.dirname(__file__) + '/' + writer + '.json', 'w') as thoughts_file:
         json.dump(thought_entries, thoughts_file)
+    with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/user/users.json') as users_file:
+        user_data = json.load(users_file)
+        for info in user_data:
+            if info['username'].lower() == writer.lower():
+                posts = int(info['post_number']) - 1
+                updated_data = {'username': info['username'], 'password': info['password'], 'salt': info['salt'], 'first_name': info['first_name'], 'last_name': info['last_name'], 'name_public': info['name_public'], 'email': info['email'], 'email_public': info['email_public'], 'color': info['color'], 'about': info['about'], 'admin': info['admin'], 'member_since': info['member_since'], 'shapes_plays': info['shapes_plays'], 'shapes_scores': info['shapes_scores'], 'shapes_high_score': info['shapes_high_score'], 'rhythm_plays': info['rhythm_plays'], 'rhythm_scores': info['rhythm_scores'], 'rhythm_high_score': info['rhythm_high_score'], 'rhythm_high_lifespan': info['rhythm_high_lifespan'], 'images': info['images'], 'liked_images': info['liked_images'], 'post_number': posts}
+                user_data = [info for info in user_data if info['username'].lower() != writer.lower()]
+        user_data.append(updated_data)
+        updated_data = user_data
+    with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/user/users.json', 'w') as users_file:
+        json.dump(updated_data, users_file)
     return make_response('Success', 200)
 
 def get_entries():
