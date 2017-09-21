df39873494e5c8c563ab65f0495f08fa535c1457
estherh5 <hoffman.esther@gmail.com>
2017-09-20 20:10:52 -0400

    Add timestamp to leader entry

--- rhythm_of_life/rhythm_of_life.py
+++ rhythm_of_life/rhythm_of_life.py
@@ -9,12 +9,14 @@ from user import user
 
 def add_leader():
     data = request.get_json()
+    timestamp = json.dumps(datetime.now(timezone.utc), default = user.timeconvert)
     if data['status'] == 'user':
         verification = user.verify_token()
         if verification.status.split(' ')[0] != '200':
             return make_response('Could not verify', 401)
         payload = json.loads(verification.data.decode())
         player = payload['username']
+        leader_entry = {'timestamp': timestamp, 'score': data['score'], 'lifespan': data['lifespan'], 'name': player, 'status': data['status']}
         with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/user/users.json') as users_file:
             user_data = json.load(users_file)
             for info in user_data:
@@ -26,19 +28,21 @@ def add_leader():
                     else:
                         score = info['rhythm_high_score']
                         lifespan = info['rhythm_high_lifespan']
-                    score_data = {'date': json.dumps(datetime.now(timezone.utc), default = user.timeconvert), 'score': data['score'], 'lifespan': data['lifespan']}
+                    score_entry = {'timestamp': json.dumps(datetime.now(timezone.utc), default = user.timeconvert), 'score': data['score'], 'lifespan': data['lifespan']}
                     scores = info['rhythm_scores']
-                    scores.append(score_data)
+                    scores.append(score_entry)
                     scores.sort(key = itemgetter('score'), reverse = True)
-                    updated_data = {'username': info['username'], 'password': info['password'], 'salt': info['salt'], 'first_name': info['first_name'], 'last_name': info['last_name'], 'name_public': info['name_public'], 'email': info['email'], 'email_public': info['email_public'], 'background_color': info['background_color'], 'color': info['color'], 'about': info['about'], 'admin': info['admin'], 'member_since': info['member_since'], 'shapes_plays': info['shapes_plays'], 'shapes_scores': info['shapes_scores'], 'shapes_high_score': info['shapes_high_score'], 'rhythm_plays': plays, 'rhythm_scores': scores, 'rhythm_high_score': score, 'rhythm_high_lifespan': lifespan, 'images': info['images'], 'liked_images': info['liked_images'], 'post_number': info['post_number']}
+                    updated_data = {'username': info['username'], 'password': info['password'], 'salt': info['salt'], 'first_name': info['first_name'], 'last_name': info['last_name'], 'name_public': info['name_public'], 'email': info['email'], 'email_public': info['email_public'], 'background_color': info['background_color'], 'color': info['color'], 'about': info['about'], 'admin': info['admin'], 'member_since': info['member_since'], 'shapes_plays': info['shapes_plays'], 'shapes_scores': info['shapes_scores'], 'shapes_high_score': info['shapes_high_score'], 'rhythm_plays': plays, 'rhythm_scores': scores, 'rhythm_high_score': score, 'rhythm_high_lifespan': lifespan, 'drawings': info['drawings'], 'liked_drawings': info['liked_drawings'], 'post_number': info['post_number']}
                     user_data = [info for info in user_data if info['username'].lower() != player.lower()]
             user_data.append(updated_data)
             updated_data = user_data
         with open(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/user/users.json', 'w') as users_file:
             json.dump(updated_data, users_file)
+    else:
+        leader_entry = {'timestamp': timestamp, 'score': data['score'], 'lifespan': data['lifespan'], 'name': data['name'], 'status': data['status']}
     with open(os.path.dirname(__file__) + '/leaders.json') as leaders_file:
         content = json.load(leaders_file)
-        content.append(data)
+        content.append(leader_entry)
     with open(os.path.dirname(__file__) + '/leaders.json', 'w') as leaders_file:
         json.dump(content, leaders_file)
     return make_response('Success!', 200)
