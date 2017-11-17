3f78832efaeb979dd82f8c0923af9eae4bee03b6
estherh5 <hoffman.esther@gmail.com>
2017-11-17 04:10:46 -0500

    Move login function to user.py file
    - Add comments explaining what each endpoint function requires from client
    - Replace PUT methods with PATCH for endpoint functions that update resources
    - Add spacing between chunks of code for readability

--- server.py
+++ server.py
@@ -1,178 +1,234 @@
-import base64
-import hashlib
-import hmac
-import json
-import math
 import os
 
-from base64 import b64encode
 from canvashare import canvashare
-from flask import Flask, make_response, request
+from flask import Flask, request
 from flask_cors import CORS
-from hashlib import sha256, sha512
-from math import floor
 from rhythm_of_life import rhythm_of_life
 from shapes_in_rain import shapes_in_rain
 from thought_writer import thought_writer
-from time import time
 from user import user
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
-# Only allow Production server access for requests coming from api.crystalprism.io
-if os.environ['ENV_TYPE'] == 'Production':
-    cors = CORS(app, resources = {r"/api/*": {"origins": r"https://api.crystalprism.io"}})
+
+# Only allow Production server access for requests coming from
+# api.crystalprism.io
+if os.environ['ENV_TYPE'] == 'Prod':
+    prod_resource = {r"/api/*": {"origins": r"https://api.crystalprism.io"}}
+    cors = CORS(app, resources = prod_resource)
 elif os.environ['ENV_TYPE'] == 'Dev':
     cors = CORS(app, resources = {r"/api/*": {"origins": "*"}})
-
-
-@app.route('/api/user', methods = ['POST', 'GET', 'PUT', 'DELETE'])
-def user_info():
-    if request.method == 'POST':
-        return user.create_user()
-    if request.method == 'GET':
-        return user.read_user()
-    if request.method == 'PUT':
-        return user.update_user()
-    if request.method == 'DELETE':
-        return user.delete_user()
-
-
-@app.route('/api/user/<username>', methods = ['GET'])
-def user_info_public(username):
-    if request.method == 'GET':
-        return user.read_user_public(username)
-
-
-@app.route('/api/user/verify', methods = ['GET'])
-def verify_user_token():
-    if request.method == 'GET':
-        return user.verify_token()
-
-
-@app.route('/api/users', methods = ['GET'])
-def users_info():
-    if request.method == 'GET':
-        return user.read_users()
-
-
-@app.route('/api/login', methods = ['GET'])
-def login_route():
-    if request.method == 'GET':
-        data = request.authorization
-        username = data.username
-        password = data.password
-        # Check that authorization request contains required data (header, username, password)
-        if not data or not data.username or not data.password:
-            return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-        with open(os.path.dirname(__file__) + '/user/users.json', 'r') as users_file:
-            users = json.load(users_file)
-            for user_data in users:
-                if user_data['username'].lower() == username.lower():
-                    # Reject requests for logging into deleted user accounts
-                    if user_data['status'] == 'deleted':
-                        return make_response('Username does not exist', 400)
-                    # Check requested password against stored hashed and salted password
-                    salt = user_data['salt'].encode()
-                    password = password.encode()
-                    hashed_password = sha512(salt + password).hexdigest()
-                    # Generate JWT token if password is correct
-                    if user_data['password'] == hashed_password:
-                        header = b'{"alg": "HS256", "typ": "JWT"}'
-                        payload = json.dumps({'username': username, 'exp': math.floor(time() + (60 * 60))}).encode()
-                        secret = b'MySecret'
-                        message = base64.urlsafe_b64encode(header) + b'.' + base64.urlsafe_b64encode(payload)
-                        signature = hmac.new(secret, message, digestmod = hashlib.sha256).digest()
-                        signature = base64.urlsafe_b64encode(signature)
-                        token = message + b'.' + signature
-                        response = {'token': token.decode()}
-                        return json.dumps(response)
-                    else:
-                        return make_response('Incorrect password', 400)
-            return make_response('Username does not exist', 400)
+    app.config['DEBUG'] = True
 
 
 @app.route('/api/canvashare/drawing', methods = ['POST'])
 def drawing():
+    # Post a drawing when client sends the jsonified drawing data URI in base64
+    # format and drawing title in the request body and a verified bearer token
+    # in the request Authorization header
     if request.method == 'POST':
         return canvashare.create_drawing()
 
 
 @app.route('/api/canvashare/drawing/<artist>/<drawing_file>', methods = ['GET'])
 def get_drawing(artist, drawing_file):
+    # Retrieve a drawing PNG file when client sends the artist's username and
+    # drawing file name (e.g., '1.png') in the request URL; no bearer token
+    # needed
     if request.method == 'GET':
         return canvashare.read_drawing(artist, drawing_file)
 
 
-@app.route('/api/canvashare/drawing-info/<artist>/<drawing_id>', methods = ['PUT', 'GET'])
+@app.route('/api/canvashare/drawing-info/<artist>/<drawing_id>',
+    methods = ['PATCH', 'GET'])
 def drawing_info(artist, drawing_id):
+    # Retrieve an artist's drawing's attributes when client sends the artist's
+    # username and drawing file name without the extension (e.g., '1') in the
+    # request URL; no bearer token needed
     if request.method == 'GET':
         return canvashare.read_drawing_info(artist, drawing_id)
-    if request.method == 'PUT':
+
+    # Update a drawing's attributes when client sends the artist's username and
+    # drawing file name without the extension (e.g., '1') in the request URL
+    # and jsonified attribute request ('like', 'unlike', 'view') in request
+    # body and verified bearer token in request Authorization header
+    if request.method == 'PATCH':
         return canvashare.update_drawing_info(artist, drawing_id)
 
 
 @app.route('/api/canvashare/gallery', methods = ['GET'])
 def gallery():
+    # Retrieve all users' drawing file paths as '[artist]/[drawing_id].png',
+    # in order of newest to oldest drawings; no bearer token needed; query
+    # params specify number of drawings
     if request.method == 'GET':
         return canvashare.read_all_drawings()
 
 
 @app.route('/api/canvashare/gallery/<artist>', methods = ['GET'])
 def user_gallery(artist):
+    # Retrieve a user's drawing file paths as '[artist]/[drawing_name].png', in
+    # order of newest to oldest drawings, when client sends the artist's
+    # username in the request URL; no bearer token needed; query params specify
+    # number of drawings
     if request.method == 'GET':
         return canvashare.read_all_user_drawings(artist)
 
 
-@app.route('/api/shapes-in-rain', methods = ['POST', 'GET'])
-def shapes_leaders():
-    if request.method == 'POST':
-        return shapes_in_rain.create_leader()
+@app.route('/api/login', methods = ['GET'])
+def login_route():
+    # Check if username and password in request Authorization header match
+    # username and password stored for a user account and return JWT if so
     if request.method == 'GET':
-        return shapes_in_rain.read_leaders()
+        return user.login()
 
 
 @app.route('/api/rhythm-of-life', methods = ['POST', 'GET'])
 def rhythm_leaders():
+    # Post a game score for a user when client sends the jsonified score and
+    # lifespan in the request body and verified bearer token in request
+    # Authorization header
     if request.method == 'POST':
         return rhythm_of_life.create_leader()
+
+    # Retrieve all users' game scores, in order of highest to lowest score; no
+    # bearer token needed; query params specify number of scores
     if request.method == 'GET':
         return rhythm_of_life.read_leaders()
 
 
-@app.route('/api/thought-writer/post', methods = ['POST', 'PUT', 'DELETE'])
+@app.route('/api/shapes-in-rain', methods = ['POST', 'GET'])
+def shapes_leaders():
+    # Post a game score for a user when client sends the jsonified score in the
+    # request body and verified bearer token in request Authorization header
+    if request.method == 'POST':
+        return shapes_in_rain.create_leader()
+
+    # Retrieve all users' game scores, in order of highest to lowest score; no
+    # bearer token needed; query params specify number of scores
+    if request.method == 'GET':
+        return shapes_in_rain.read_leaders()
+
+
+@app.route('/api/thought-writer/post', methods = ['POST', 'PATCH', 'DELETE'])
 def post():
+    # Post a thought post when client sends the jsonified post content, title,
+    # and public status ('true' or 'false') in the request body and a verified
+    # bearer token in the request Authorization header
     if request.method == 'POST':
         return thought_writer.create_post()
-    if request.method == 'PUT':
+
+    # Update a thought post when client sends the jsonified post content, post
+    # creation timestamp (UTC), title, and public status ('true' or 'false') in
+    # the request body and a verified bearer token in the request Authorization
+    # header
+    if request.method == 'PATCH':
         return thought_writer.update_post()
+
+    # Delete a thought post when client sends the jsonified post creation
+    # timestamp (UTC) in the request body and a verified bearer token in the
+    # request Authorization header
     if request.method == 'DELETE':
         return thought_writer.delete_post()
 
 
-@app.route('/api/thought-writer/post/<writer_name>/<post_timestamp>', methods = ['GET'])
+@app.route('/api/thought-writer/post/<writer_name>/<post_timestamp>',
+    methods = ['GET'])
 def get_post(writer_name, post_timestamp):
+    # Retrieve a user's thought post when client sends the writer's username and
+    # the thought post's URI-encoded creation timestamp (UTC) in the request
+    # URL; a verified bearer token must be in request Authorization header for
+    # private post to be retrieved
     if request.method == 'GET':
         return thought_writer.read_post(writer_name, post_timestamp)
 
 
-@app.route('/api/thought-writer/comment/<writer_name>/<post_timestamp>', methods = ['POST', 'PUT', 'DELETE'])
+@app.route('/api/thought-writer/comment/<writer_name>/<post_timestamp>',
+    methods = ['POST', 'PATCH', 'DELETE'])
 def comment(writer_name, post_timestamp):
+    # Post a comment to a thought post when client sends the post writer's
+    # username and the thought post's URI-encoded creation timestamp (UTC) in
+    # the request URL, the jsonified comment content in the request body, and a
+    # verified bearer token in the request Authorization header
     if request.method == 'POST':
         return thought_writer.create_comment(writer_name, post_timestamp)
-    if request.method == 'PUT':
+
+    # Update a comment to a thought post when client sends the post writer's
+    # username and the thought post's URI-encoded creation timestamp (UTC) in
+    # the request URL, the jsonified comment content and original comment
+    # creation timestamp (UTC) in the request body, and a verified bearer token
+    # in the request Authorization header
+    if request.method == 'PATCH':
         return thought_writer.update_comment(writer_name, post_timestamp)
+
+    # Delete a comment to a thought post when client sends the post writer's
+    # username and the thought post's URI-encoded creation timestamp (UTC) in
+    # the request URL, the jsonified comment creation timestamp (UTC) in the
+    # request body, and a verified bearer token in request Authorization header
     if request.method == 'DELETE':
         return thought_writer.delete_comment(writer_name, post_timestamp)
 
 
 @app.route('/api/thought-writer/post-board', methods = ['GET'])
 def post_board():
+    # Retrieve all users' public thought posts; no bearer token needed; query
+    # params specify number of posts
     if request.method == 'GET':
         return thought_writer.read_all_posts()
 
 
 @app.route('/api/thought-writer/post-board/<writer_name>', methods = ['GET'])
 def user_post_board(writer_name):
+    # Retrieve all of a single user's thought posts by specifying the writer's
+    # username in the request URL; verified bearer token for the writer is
+    # needed in the request Authorization header to send user's private and
+    # public posts; otherwise, only public posts will be sent; query params
+    # specify number of posts
     if request.method == 'GET':
         return thought_writer.read_all_user_posts(writer_name)
+
+
+@app.route('/api/user', methods = ['POST', 'GET', 'PATCH', 'DELETE'])
+def user_info_private():
+    # Create a user account when client sends the jsonified username and
+    # password in the request body
+    if request.method == 'POST':
+        return user.create_user()
+
+    # Retrieve a user's complete account information when there is a verified
+    # bearer token for the user in the request Authorization header
+    if request.method == 'GET':
+        return user.read_user()
+
+    # Update a user's account when the client sends the jsonified account
+    # updates in the request body and a verified bearer token for the user in
+    # the request Authorization header
+    if request.method == 'PATCH':
+        return user.update_user()
+
+    # Change a user's account status to deleted when there is a verified
+    # bearer token for the user in the request Authorization header
+    if request.method == 'DELETE':
+        return user.delete_user()
+
+
+@app.route('/api/user/<username>', methods = ['GET'])
+def user_info_public(username):
+    # Retrieve a user's limited account information; no bearer token needed
+    if request.method == 'GET':
+        return user.read_user_public(username)
+
+
+@app.route('/api/user/verify', methods = ['GET'])
+def verify_user_token():
+    # Check if bearer token in request Authorization header is valid and
+    # return the expiration time (in seconds since epoch) if so
+    if request.method == 'GET':
+        return user.verify_token()
+
+
+@app.route('/api/users', methods = ['GET'])
+def users_info():
+    # Retrieve all users' usernames if there is a verified bearer token in the
+    # Authorization header; query params specify number of users
+    if request.method == 'GET':
+        return user.read_users()
--- user/user.py
+++ user/user.py
@@ -1,23 +1,64 @@
-import base64
 import fcntl
-import hashlib
 import hmac
 import json
-import math
 import os
 import re
-import uuid
 
-from base64 import b64decode, b64encode
+from base64 import urlsafe_b64decode, urlsafe_b64encode
 from datetime import datetime, timezone
 from flask import jsonify, make_response, request
 from hashlib import sha256, sha512
+from math import floor
 from time import time
+from uuid import uuid4
+
+
+def login():
+    data = request.authorization
+    username = data.username
+    password = data.password
+
+    # Check that authorization request contains required data (header,
+    # username, password)
+    if not data or not data.username or not data.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    with open(os.path.dirname(__file__) + '/users.json', 'r') as users_file:
+        users = json.load(users_file)
+        for user_data in users:
+            if user_data['username'].lower() == username.lower():
+                # Reject requests for logging into deleted user accounts
+                if user_data['status'] == 'deleted':
+                    return make_response('Username does not exist', 400)
+                # Check requested password against stored hashed and salted
+                # password
+                salt = user_data['salt'].encode()
+                password = password.encode()
+                hashed_password = sha512(salt + password).hexdigest()
+                # Generate JWT token if password is correct
+                if user_data['password'] == hashed_password:
+                    header = b'{"alg": "HS256", "typ": "JWT"}'
+                    payload = json.dumps({
+                                          'username': username,
+                                          'exp': floor(time() + (60 * 60))
+                                          }).encode()
+                    secret = b'MySecret'
+                    message = urlsafe_b64encode(header) + b'.'
+                              + urlsafe_b64encode(payload)
+                    signature = hmac.new(secret, message, digestmod = sha256).digest()
+                    signature = urlsafe_b64encode(signature)
+                    token = message + b'.' + signature
+                    response = {'token': token.decode()}
+                    return json.dumps(response)
+                else:
+                    return make_response('Incorrect password', 400)
+        return make_response('Username does not exist', 400)
 
 
 def create_user():
     data = request.get_json()
     username = data['username']
+
     with open(os.path.dirname(__file__) + '/users.json', 'r') as users_file:
         # Lock file to prevent overwrite
         fcntl.flock(users_file, fcntl.LOCK_EX)
@@ -27,19 +68,49 @@ def create_user():
             if user_data['username'].lower() == username.lower():
                 return make_response('Username already exists', 400)
         # Generate random UUID as non-client-facing user identifier
-        member_id = str(uuid.uuid4())
+        member_id = str(uuid4())
         password = data['password'].encode()
         # Generate salt using CSPRNG (32 random alphanumeric characters)
         salt = b64encode(os.urandom(32))
         # Hash password with SHA512 cryptographic hash function
         hashed_password = sha512(salt + password).hexdigest()
-        entry = {'member_id': member_id, 'status': 'active', 'username': username, 'password': hashed_password, 'salt': salt.decode(), 'admin': False, 'member_since': datetime.now(timezone.utc).isoformat(), 'first_name': '', 'last_name': '', 'name_public': False, 'email': '', 'email_public': False, 'background_color': '#ffffff', 'icon_color': '#000000', 'about': '', 'shapes_plays': 0, 'shapes_scores': [], 'shapes_high_score': 0, 'rhythm_plays': 0, 'rhythm_scores': [], 'rhythm_high_score': 0, 'rhythm_high_lifespan': '00:00:00', 'drawing_number': 0, 'liked_drawings': [], 'post_number': 0, 'comment_number': 0}
+        entry = {
+                 'member_id': member_id,
+                 'status': 'active',
+                 'username': username,
+                 'password': hashed_password,
+                 'salt': salt.decode(),
+                 'admin': False,
+                 'member_since': datetime.now(timezone.utc).isoformat(),
+                 'first_name': '',
+                 'last_name': '',
+                 'name_public': False,
+                 'email': '',
+                 'email_public': False,
+                 'background_color': '#ffffff',
+                 'icon_color': '#000000',
+                 'about': '',
+                 'shapes_plays': 0,
+                 'shapes_scores': [],
+                 'shapes_high_score': 0,
+                 'rhythm_plays': 0,
+                 'rhythm_scores': [],
+                 'rhythm_high_score': 0,
+                 'rhythm_high_lifespan': '00:00:00',
+                 'drawing_number': 0,
+                 'liked_drawings': [],
+                 'post_number': 0,
+                 'comment_number': 0
+                 }
         users.append(entry)
+
     with open(os.path.dirname(__file__) + '/users.json', 'w') as users_file:
         json.dump(users, users_file)
         # Release lock on file
         fcntl.flock(users_file, fcntl.LOCK_UN)
+
     return make_response('Success', 200)
+
 
 def read_user():
     verification = verify_token()
@@ -47,16 +118,19 @@ def read_user():
         return make_response('Access denied', 403)
     payload = json.loads(verification.data.decode())
     requester = payload['username']
+
     with open(os.path.dirname(__file__) + '/users.json', 'r') as users_file:
         users = json.load(users_file)
         for user_data in users:
             if user_data['username'].lower() == requester.lower():
-                # Replace artist member_id with username for each drawing the user liked
+                # Replace artist member_id with username for each drawing the
+                # user liked
                 for i in range(len(user_data['liked_drawings'])):
                     for artist in users:
                         if artist['member_id'] == user_data['liked_drawings'][i].split('/', 1)[0]:
                             user_data['liked_drawings'][i] = str(artist['username'] + '/' + user_data['liked_drawings'][i].split('/', 1)[1])
                 return jsonify(user_data)
+
 
 def update_user():
     verification = verify_token()
@@ -67,6 +141,7 @@ def update_user():
     data = request.get_json()
     username = data['username']
     password = data['password']
+
     with open(os.path.dirname(__file__) + '/users.json', 'r') as users_file:
         # Lock file to prevent overwrite
         fcntl.flock(users_file, fcntl.LOCK_EX)
@@ -95,20 +170,27 @@ def update_user():
             # Update bearer token if user requests username update
             if username.lower() != requester.lower():
                 header = b'{"alg": "HS256", "typ": "JWT"}'
-                payload = json.dumps({'username': username, 'exp': math.floor(time() + (60 * 60))}).encode()
+                payload = json.dumps({
+                                      'username': username,
+                                      'exp': floor(time() + (60 * 60))
+                                      }).encode()
                 secret = b'MySecret'
-                message = base64.urlsafe_b64encode(header) + b'.' + base64.urlsafe_b64encode(payload)
-                signature = hmac.new(secret, message, digestmod = hashlib.sha256).digest()
-                signature = base64.urlsafe_b64encode(signature)
+                message = urlsafe_b64encode(header) + b'.'
+                          + urlsafe_b64encode(payload)
+                signature = hmac.new(secret, message, digestmod = sha256).digest()
+                signature = urlsafe_b64encode(signature)
                 token = message + b'.' + signature
                 response = token.decode()
             else:
                 response = 'Success'
+
     with open(os.path.dirname(__file__) + '/users.json', 'w') as users_file:
         json.dump(users, users_file)
         # Release lock on file
         fcntl.flock(users_file, fcntl.LOCK_UN)
+
     return make_response(response, 200)
+
 
 def delete_user():
     verification = verify_token()
@@ -116,6 +198,7 @@ def delete_user():
         return make_response('Access denied', 403)
     payload = json.loads(verification.data.decode())
     requester = payload['username']
+
     with open(os.path.dirname(__file__) + '/users.json', 'r') as users_file:
         # Lock file to prevent overwrite
         fcntl.flock(users_file, fcntl.LOCK_EX)
@@ -123,11 +206,14 @@ def delete_user():
         for user_data in users:
             if user_data['username'].lower() == requester.lower():
                 user_data['status'] = 'deleted'
+
     with open(os.path.dirname(__file__) + '/users.json', 'w') as users_file:
         json.dump(users, users_file)
         # Release lock on file
         fcntl.flock(users_file, fcntl.LOCK_UN)
+
     return make_response('Success', 200)
+
 
 def read_user_public(username):
     with open(os.path.dirname(__file__) + '/users.json', 'r') as users_file:
@@ -144,48 +230,76 @@ def read_user_public(username):
                     email = user_data['email']
                 else:
                     email = ''
-                data = {'name': name, 'email': email, 'background_color': user_data['background_color'], 'icon_color': user_data['icon_color'], 'about': user_data['about'], 'member_since': user_data['member_since'], 'shapes_high_score': user_data['shapes_high_score'], 'rhythm_high_lifespan': user_data['rhythm_high_lifespan'], 'drawing_number': user_data['drawing_number'], 'post_number': user_data['post_number'], 'comment_number': user_data['comment_number']}
+                data = {
+                        'username': user_data['username'],
+                        'name': name,
+                        'email': email,
+                        'background_color': user_data['background_color'],
+                        'icon_color': user_data['icon_color'],
+                        'about': user_data['about'],
+                        'member_since': user_data['member_since'],
+                        'shapes_high_score': user_data['shapes_high_score'],
+                        'rhythm_high_lifespan': user_data['rhythm_high_lifespan'],
+                        'drawing_number': user_data['drawing_number'],
+                        'post_number': user_data['post_number'],
+                        'comment_number': user_data['comment_number']
+                        }
                 return jsonify(data)
         return make_response('Username does not exist', 400)
+
 
 def verify_token():
     data = request.headers.get('Authorization')
     if not data:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
     token = data.split(' ')[1]
+
     # Check if token in Authorization header is properly formatted
     pattern = re.compile(r'^[a-zA-Z0-9-_]+={0,2}\.[a-zA-Z0-9-_]+={0,2}\.[a-zA-Z0-9-_]+={0,2}$')
     if not pattern.match(token):
         return make_response('Token is incorrect format', 401)
-    header = base64.urlsafe_b64decode(token.split('.')[0])
-    payload = json.loads(base64.urlsafe_b64decode(token.split('.')[1]).decode())
+
+    header = urlsafe_b64decode(token.split('.')[0])
+    payload = json.loads(urlsafe_b64decode(token.split('.')[1]).decode())
+
     # Check if token is past expiration time
     if payload['exp'] < time():
         return make_response('Token expired', 401)
-    signature = base64.urlsafe_b64decode(token.split('.')[2])
-    # Generate signature using secret to check against signature from Auth header
+
+    signature = urlsafe_b64decode(token.split('.')[2])
+
+    # Generate signature using secret to check against signature from Auth
+    # header
     secret = b'MySecret'
-    message = base64.urlsafe_b64encode(header) + b'.' + base64.urlsafe_b64encode(json.dumps(payload).encode())
-    signature_check = hmac.new(secret, message, digestmod = hashlib.sha256).digest()
+    message = urlsafe_b64encode(header) + b'.'
+              + urlsafe_b64encode(json.dumps(payload).encode())
+    signature_check = hmac.new(secret, message, digestmod = sha256).digest()
     if signature != signature_check:
         return make_response('Token compromised', 401)
+
     return make_response(json.dumps(payload).encode(), 200)
+
 
 def read_users():
     # Get number of requested users from query parameters
     if request.args.get('start') is not None:
         request_start = int(request.args.get('start'))
         request_end = int(request.args.get('end'))
+
     # Set default number of users if not specified in query parameters
     else:
         request_start = 0
         request_end = 9
+
     verification = verify_token()
+
     # Return list of usernames for logged-in user
     if verification.status.split(' ')[0] == '200':
         with open(os.path.dirname(__file__) + '/users.json', 'r') as users_file:
             users = json.load(users_file)
             usernames = [user_data['username'] for user_data in users[request_start:request_end]]
             return jsonify(usernames[request_start:request_end])
+
     else:
         return make_response('Access denied', 403)
