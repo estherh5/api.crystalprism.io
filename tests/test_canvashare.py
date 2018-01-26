0b91b158967199938e136b2c789d6ba0d4acd7f1
estherh5 <hoffman.esther@gmail.com>
2018-01-25 19:28:33 -0500

    Add tests for CanvaShare gallery endpoint [GET]

---
+++ tests/test_canvashare.py
@@ -0,0 +1,64 @@
+import json
+import unittest
+
+from server import app
+
+
+class TestGallery(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_gallery_get(self):
+        # Act
+        response = self.client.get('/api/canvashare/gallery')
+        response_data = json.loads(response.get_data(as_text=True))
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response_data), 10)
+        self.assertEqual(response_data, ['user/1.png'])
+
+    def test_gallery_get_none(self):
+        # Arrange
+        data = {'start': 50}
+
+        # Act
+        response = self.client.get(
+            '/api/canvashare/gallery',
+            query_string=data, # data=data for self.client.post()
+            content_type='application/json'
+            )
+        response_data = json.loads(response.get_data(as_text=True))
+
+        # Assert
+        self.assertEqual(response_data, [])
+
+    def test_gallery_get_five(self):
+        # Arrange
+        data = {'end': 5}
+
+        # Act
+        response = self.client.get(
+            '/api/canvashare/gallery',
+            query_string=data, # data=data for self.client.post()
+            content_type='application/json'
+            )
+        response_data = json.loads(response.get_data(as_text=True))
+
+        # Assert
+        self.assertEqual(len(response_data), 5)
+
+    def test_gallery_get_error(self):
+        # Arrange
+        data = {'start': 5, 'end': 0}
+
+        # Act
+        response = self.client.get(
+            '/api/canvashare/gallery',
+            query_string=data, # data=data for self.client.post()
+            content_type='application/json'
+            )
+        response_data = json.loads(response.get_data(as_text=True))
+
+        # Assert
+        self.assertEqual(response_data, [])
