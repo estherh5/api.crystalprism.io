df9c33dcf8a5f56281020fc478835b116a29e34e
estherh5 <hoffman.esther@gmail.com>
2018-02-10 14:04:21 -0500

    Add tests for `/api/thought-writer/post` endpoint
    Add tests for POST, GET, PATCH, DELETE methods

---
+++ tests/test_thought_writer.py
@@ -0,0 +1,118 @@
+import json
+import re
+
+from server import app
+from utils.tests import CrystalPrismTestCase
+
+
+# Test /api/thought-writer/post endpoint [POST, GET, PATCH, DELETE]
+class TestPost(CrystalPrismTestCase):
+    def test_post_post_get_patch_and_delete(self):
+        # Arrange [POST]
+        # Create user and login to get token for Authorization header
+        self.create_user()
+        self.login()
+        header = {'Authorization': 'Bearer ' + self.token}
+        post_data = {'title': 'Test', 'content': 'Test', 'public': False}
+
+        # Act [POST]
+        post_response = self.client.post(
+            '/api/thought-writer/post',
+            headers=header,
+            data=json.dumps(post_data),
+            content_type='application/json'
+            )
+        timestamp = post_response.get_data(as_text=True)
+
+        # Assert [POST]
+        self.assertEqual(post_response.status_code, 201)
+
+        # Ensure timestamp matches UTC format
+        timestamp_pattern = re.compile(
+            r'\d{4}-[0-1]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-6]\d.\d{6}\+\d\d:\d\d'
+            )
+        self.assertEqual(bool(timestamp_pattern.match(timestamp)), True)
+
+        # Act [GET]
+        get_response = self.client.get(
+            '/api/thought-writer/post/' + self.username + '/' + timestamp,
+            headers=header
+            )
+        post = json.loads(get_response.get_data(as_text=True))
+
+        # Assert [GET]
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(post['title'], 'Test')
+        self.assertEqual(bool(timestamp_pattern.match(
+            post['timestamp'])), True
+            )
+        self.assertEqual(post['content'], 'Test')
+        self.assertEqual(post['public'], False)
+        self.assertEqual(post['comments'], [])
+
+        # Arrange [PATCH]
+        patch_data = {'title': 'Test 2', 'timestamp': timestamp,
+            'content': 'Test 2', 'public': True}
+
+        # Act [PATCH]
+        patch_response = self.client.patch(
+            '/api/thought-writer/post',
+            headers=header,
+            data=json.dumps(patch_data),
+            content_type='application/json'
+            )
+
+        patched_get_response = self.client.get(
+            '/api/thought-writer/post/' + self.username + '/' + timestamp,
+            headers=header
+            )
+        patched_post = json.loads(patched_get_response.get_data(as_text=True))
+
+        # Assert [PATCH]
+        self.assertEqual(patch_response.status_code, 200)
+        self.assertEqual(patched_get_response.status_code, 200)
+        self.assertEqual(patched_post['title'], 'Test 2')
+        self.assertEqual(bool(timestamp_pattern.match(
+            patched_post['timestamp'])), True
+            )
+        self.assertEqual(patched_post['content'], 'Test 2')
+        self.assertEqual(patched_post['public'], True)
+        self.assertEqual(patched_post['comments'], [])
+
+        # Arrange [DELETE]
+        delete_data = {'timestamp': timestamp}
+
+        # Act [DELETE]
+        delete_response = self.client.delete(
+            '/api/thought-writer/post',
+            headers=header,
+            data=json.dumps(delete_data),
+            content_type='application/json'
+            )
+
+        deleted_get_response = self.client.get(
+            '/api/thought-writer/post/' + self.username + '/' + timestamp,
+            headers=header
+            )
+
+        # Assert [DELETE]
+        self.assertEqual(delete_response.status_code, 200)
+        self.assertEqual(deleted_get_response.status_code, 404)
+
+    def test_public_post_get(self):
+        writer_name = 'user'
+        timestamp = '2017-10-05T00:00:00.000000+00:00'
+
+        # Act
+        get_response = self.client.get(
+            '/api/thought-writer/post/' + writer_name + '/' + timestamp
+            )
+        post = json.loads(get_response.get_data(as_text=True))
+
+        # Assert [POST]
+        self.assertEqual(get_response.status_code, 200)
+        self.assertEqual(post['writer'], 'user')
+        self.assertEqual(post['title'], 'Welcome')
+        self.assertEqual(post['timestamp'], timestamp)
+        self.assertEqual('Welcome to Thought Writer' in post['content'], True)
+        self.assertEqual(post['comments'], [])
