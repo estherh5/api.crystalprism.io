ad0ece2a58900cd6d442abc6d23c5c3283be9738
estherh5 <hoffman.esther@gmail.com>
2018-02-24 13:35:18 -0500

    Add `/api/photos` [GET] endpoint
    - Add endpoint for getting photos from Amazon S3 bucket
    - Add tests for `/api/photos` [GET] endpoint

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
+import boto3
 import json
 import os
 
-from flask import Flask, make_response, request
+from flask import Flask, jsonify, make_response, request
 from flask_cors import CORS
 
 from canvashare import canvashare
@@ -95,6 +96,33 @@ def login_route():
     # username and password stored for a user account and return JWT if so
     if request.method == 'GET':
         return user.login()
+
+
+@app.route('/api/photos', methods=['GET'])
+def photos():
+    # Retrieve URLs for photos stored on Amazon S3 crystalprism-photos bucket;
+    # environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY must
+    # be set from AWS account; query params specify number of URLs
+    if request.method == 'GET':
+        # Get number of requested photos from query parameters, using default
+        # if null
+        request_start = int(request.args.get('start', 0))
+        request_end = int(request.args.get('end', request_start + 10))
+
+        # Return error if start query parameter is greater than end
+        if request_start > request_end:
+            return make_response('Start param cannot be greater than end', 400)
+
+        urls = []
+
+        s3 = boto3.resource('s3')
+        bucket_name = os.environ['S3_BUCKET']
+        bucket = s3.Bucket(bucket_name)
+
+        for item in bucket.objects.all():
+            urls.append(os.environ['PHOTO_URL_START'] + item.key)
+
+        return jsonify(urls[request_start:request_end])
 
 
 @app.route('/api/ping', methods=['GET'])
---
+++ tests/test_photos.py
@@ -0,0 +1,62 @@
+import boto3
+import json
+import os
+
+from server import app
+from unittest.mock import patch
+from utils.tests import CrystalPrismTestCase
+
+
+# Test /api/photos endpoint [GET]
+class TestPhotos(CrystalPrismTestCase):
+    class MockObjectSummary(object):
+        def __init__(self, key):
+            self.key = key
+
+    @patch('server.boto3')
+    def test_photos_get(self, boto3):
+        # Arrange
+        url_start = os.environ['PHOTO_URL_START']
+        resource = boto3.resource.return_value
+        bucket = resource.Bucket.return_value
+        bucket.objects.all.return_value = [
+            self.MockObjectSummary('1.png'),
+            self.MockObjectSummary('10.png'),
+            self.MockObjectSummary('2.png'),
+            self.MockObjectSummary('3.png'),
+            self.MockObjectSummary('4.png'),
+            self.MockObjectSummary('5.png'),
+            self.MockObjectSummary('6.png'),
+            self.MockObjectSummary('7.png'),
+            self.MockObjectSummary('8.png'),
+            self.MockObjectSummary('9.png')
+            ]
+
+        # Act
+        response = self.client.get('/api/photos')
+        response_data = json.loads(response.get_data(as_text=True))
+
+        # Assert
+        boto3.resource.return_value.Bucket.assert_called_with(
+            os.environ['S3_BUCKET']
+            )
+
+        self.assertEqual(len(response_data), 10)
+        self.assertEqual(all(url_start in url for url in response_data),
+            True)
+
+    @patch('server.boto3')
+    def test_photos_get_error(self, boto3):
+        # Arrange
+        data = {'start': 5, 'end': 0}
+
+        # Act
+        response = self.client.get(
+            '/api/photos',
+            query_string=data
+            )
+        error = response.get_data(as_text=True)
+
+        # Assert
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(error, 'Start param cannot be greater than end')
