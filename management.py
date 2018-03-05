cf1e252d8d936ec596d4aef3fe6d675d160a7c90
estherh5 <hoffman.esther@gmail.com>
2018-03-05 12:30:32 -0500

    Add function to load initial database data
    - Update .travis.yml and server.wsgi files to reflect new environment variables
    - Update `/api/photos` endpoint function to reflect new environment variables

--- management.py
+++ management.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import argparse
+import bcrypt
+import getpass
+import json
 import os
 import psycopg2 as pg
+
+from base64 import decodebytes
 
 
 def initialize_database():
@@ -103,9 +108,236 @@ def initialize_database():
     return
 
 
-# Add argument for initializing database in CLI
+def create_owner_user(username):
+    # Set up database connection with environment variable
+    conn = pg.connect(os.environ['DB_CONNECTION'])
+
+    cursor = conn.cursor()
+
+    # Check if username already exists in database
+    cursor.execute(
+        """
+        SELECT exists (
+        SELECT 1 FROM cp_user WHERE LOWER(username) = %(username)s LIMIT 1);
+        """,
+        {'username': username.lower()}
+        )
+
+    if cursor.fetchone()[0]:
+        cursor.close()
+        conn.close()
+
+        print('Username already exists')
+
+        return
+
+    # Generate hashed password with bcrypt cryptographic hash function and salt
+    password = getpass.getpass('Enter password for ' + username + ': ')
+    password = password.encode()
+    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    # Add user account to database
+    cursor.execute(
+        """
+        INSERT INTO cp_user (username, is_admin, is_owner, password)
+        VALUES (%(username)s, %(is_admin)s, %(is_owner)s, %(password)s);
+        """,
+        {'username': username,
+        'is_admin': True,
+        'is_owner': True,
+        'password': hashed_password.decode()}
+        )
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    print('User "' + username + '" added to database.')
+
+    return
+
+
+def create_admin_user(username):
+    # Set up database connection with environment variable
+    conn = pg.connect(os.environ['DB_CONNECTION'])
+
+    cursor = conn.cursor()
+
+    # Check if username already exists in database
+    cursor.execute(
+        """
+        SELECT exists (
+        SELECT 1 FROM cp_user WHERE LOWER(username) = %(username)s LIMIT 1);
+        """,
+        {'username': username.lower()}
+        )
+
+    if cursor.fetchone()[0]:
+        cursor.close()
+        conn.close()
+
+        print('Username already exists')
+
+        return
+
+    # Generate hashed password with bcrypt cryptographic hash function and salt
+    password = getpass.getpass('Enter password for ' + username + ': ')
+    password = password.encode()
+    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    # Add user account to database
+    cursor.execute(
+        """
+        INSERT INTO cp_user (username, is_admin, password)
+        VALUES (%(username)s, %(is_admin)s, %(password)s);
+        """,
+        {'username': username,
+        'is_admin': True,
+        'password': hashed_password.decode()}
+        )
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    print('Admin user "' + username + '" added to database.')
+
+    return
+
+
+def create_posts(username, posts_filename):
+    # Open posts_filename to get initial post data
+    posts_file = 'fixtures/' + posts_filename
+    with open(posts_file, 'r') as post_data:
+        posts = json.load(post_data)
+
+    # Set up database connection with environment variable
+    conn = pg.connect(os.environ['DB_CONNECTION'])
+
+    cursor = conn.cursor()
+
+    # Add posts to database
+    for post in posts:
+        cursor.execute(
+            """
+            INSERT INTO post (member_id, content, title, public)
+            VALUES ((SELECT member_id FROM cp_user
+            WHERE LOWER(username) = %(username)s), %(content)s, %(title)s,
+            %(public)s)
+            RETURNING post_id;
+            """,
+            {'username': username.lower(),
+            'content': post['content'],
+            'title': post['title'],
+            'public': post['public']}
+            )
+
+        conn.commit()
+
+        post['post_id'] = cursor.fetchone()[0]
+
+        print('Post "' + str(post['post_id']) + '" added to database.')
+
+    cursor.close()
+    conn.close()
+
+    return
+
+
+def create_drawings(username, drawings_filename):
+    # Open drawings_filename to get initial drawing data
+    drawings_file = 'fixtures/' + drawings_filename
+    with open(drawings_file, 'r') as drawing_data:
+        drawings = json.load(drawing_data)
+
+    # Set up database connection with environment variable
+    conn = pg.connect(os.environ['DB_CONNECTION'])
+
+    cursor = conn.cursor()
+
+    # Add drawings to database
+    for drawing in drawings:
+        cursor.execute(
+            """
+            INSERT INTO drawing (member_id, title, url)
+            VALUES ((SELECT member_id FROM cp_user
+            WHERE LOWER(username) = %(username)s), %(title)s, %(url)s)
+            RETURNING drawing_id;
+            """,
+            {'username': username.lower(),
+            'title': drawing['title'],
+            'url': drawing['url']}
+            )
+
+        conn.commit()
+
+        drawing_id = cursor.fetchone()[0]
+
+        # Remove 'data:image/png;base64' from image data URL
+        drawing['url'] = drawing['url'].split(',')[1].encode('utf-8')
+
+        # Upload drawing to S3 bucket
+        s3 = boto3.resource('s3')
+        bucket_name = os.environ['S3_BUCKET']
+        bucket = s3.Bucket(bucket_name)
+        bucket_folder = os.environ['S3_CANVASHARE_DIR']
+
+        drawing_name = str(drawing_id) + '.png'
+
+        bucket.put_object(
+            Key=bucket_folder + drawing_name,
+            Body=decodebytes(drawing['url'])
+            )
+
+        # Add drawing URL to database
+        cursor.execute(
+            """
+            UPDATE drawing SET url = %(url)s WHERE drawing_id = %(drawing_id)s;
+            """,
+            {'url': os.environ['S3_URL'] + bucket_folder + drawing_name,
+            'drawing_id': drawing_id}
+            )
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        print('Drawing "' + str(drawing_id) + '" added to database.')
+
+    return
+
+
+def load_initial_data():
+    # Create webpage owner user
+    user = input('Enter username for webpage owner: ')
+    create_owner_user(user)
+
+    # Create admin user
+    admin = input('Enter username for admin user: ')
+    create_admin_user(admin)
+
+    # Create initial Thought Writer posts
+    create_posts(admin, 'thought-writer-posts.json')
+
+    # Create initial homepage post
+    create_posts(user, 'homepage-post.json')
+
+    # Create initial drawing
+    create_drawings(admin, 'canvashare-drawing.json')
+
+    print('Initial database data loaded successfully.')
+
+    return
+
+
+# Add arguments for initializing database in CLI
 parser = argparse.ArgumentParser(description='Management commands')
 parser.add_argument('action', type=str, help="an action for the database")
 args = parser.parse_args()
 if args.action == 'init_db':
     initialize_database()
+if args.action == 'load_data':
+    load_initial_data()
--- server.wsgi
+++ server.wsgi
@@ -8,12 +8,14 @@ sys.path.insert(0, '/var/www/html/api.crystalprism.io')  # Server path
 def application(environ, start_response):
     os.environ['ENV_TYPE'] = environ.get('ENV_TYPE', '')
     os.environ['SECRET_KEY'] = environ.get('SECRET_KEY', '')
-    os.environ['S3_BUCKET'] = environ.get('S3_BUCKET', '')
-    os.environ['PHOTO_URL_START'] = environ.get('PHOTO_URL_START', '')
     os.environ['AWS_ACCESS_KEY_ID'] = environ.get('AWS_ACCESS_KEY_ID', '')
     os.environ['AWS_SECRET_ACCESS_KEY'] = environ.get(
         'AWS_SECRET_ACCESS_KEY', ''
         )
+    os.environ['S3_BUCKET'] = environ.get('S3_BUCKET', '')
+    os.environ['S3_URL'] = environ.get('S3_URL', '')
+    os.environ['S3_PHOTO_DIR'] = environ.get('S3_PHOTO_DIR', '')
+    os.environ['S3_CANVASHARE_DIR'] = environ.get('S3_CANVASHARE_DIR', '')
     os.environ['DB_CONNECTION'] = environ.get('DB_CONNECTION', '')
     os.environ['DB_NAME'] = environ.get('DB_NAME', '')
     from server import app as _application
